cd0fbe0eeb12400a58394cf1093a4657efa3034f
Mariano <mariano.a.telesca@gmail.com>
2024-09-30 21:01:58 -0300

    feat: creación dinámica del mensaje disparado por los botones

--- app/src/main/java/com/example/parcial_1_am_acn4av_telesca/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_telesca/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.parcial_1_am_acn4av_telesca;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,11 +16,106 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout layout_mensaje;
+    Button boton_tenis;
+    Button boton_futbol;
+    Button boton_basket;
+    Button boton_parrilla;
+    Button boton_cumpleanios;
+    Button boton_gimnasio;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        layout_mensaje = findViewById(R.id.layout_mensaje);
+
+        boton_tenis = findViewById(R.id.boton_tenis);
+        boton_tenis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
+        boton_futbol = findViewById(R.id.boton_futbol);
+        boton_futbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
+        boton_basket = findViewById(R.id.boton_basket);
+        boton_basket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
+        boton_parrilla = findViewById(R.id.boton_parrilla);
+        boton_parrilla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
+        boton_cumpleanios = findViewById(R.id.boton_cumpleanios);
+        boton_cumpleanios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
+        boton_gimnasio = findViewById(R.id.boton_gimnasio);
+        boton_gimnasio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_mensaje.removeAllViews();
+                layout_mensaje.setVisibility(View.VISIBLE);
+                TextView mensaje = new TextView(MainActivity.this);
+                mensaje.setText(R.string.mensaje_proximamente);
+                mensaje.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                layout_mensaje.addView(mensaje);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /*
     public void mostrarMensajeProximamente(View v)
     {
         TextView text_view_mensaje_prox = findViewById(R.id.mensaje_proximamente);
@@ -35,5 +135,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+     */
 }
